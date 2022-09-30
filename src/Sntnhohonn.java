1a2acfdef7703b5ed5caac3ccd07a87c3b960c0d
0709nvanh <vananh25070901@gmail.com>
2022-09-30 15:25:43 +0700

    update

--- src/Sntnhohonn.java
+++ src/Sntnhohonn.java
@@ -4,20 +4,18 @@ public class Sntnhohonn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true) {
-            int num1,num2, numND, resultND = 0;
-            System.out.println("Mời bạn nhập vào số thứ nhất: ");
+            int num1, numND, resultND = 0;
+            System.out.println("Mời bạn nhập vào số cần check ngược đảo: ");
             num1 = Integer.parseInt(sc.nextLine());
 
-            System.out.println("Mời bạn nhập vào số thứ hai: ");
-            num2 = Integer.parseInt(sc.nextLine());
-            numND = num2;
+            numND = num1;
             while(numND != 0) {
-                int digit = numND % 10;
-                resultND = resultND * 10 + digit;
+                int phanDu = numND % 10;
+                resultND = resultND * 10 + phanDu;
                 numND /= 10;
             }
-            System.out.println(num1 == resultND ? "Số " + num2 + " là số ngược của số " + num1 : "Số " + num2 +
-                    " không phải ngược của số " + num1);
+            System.out.println(num1 == resultND ? "Số " + num1 + " là số ngược đảo " : "Số " + num1 +
+                    " không phải là số ngược đảo ");
         }
     }
 }
